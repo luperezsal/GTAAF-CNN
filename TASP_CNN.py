6b0a54a681860d8c2d346f81dc3b396d28ba59db
Luis <balumba55@gmail.com>
2022-04-13 16:19:59 +0200

    [TEST] Show DGX bug

--- TASP_CNN.py
+++ TASP_CNN.py
@@ -741,49 +741,49 @@ from hyperopt import STATUS_OK, Trials, fmin, hp, tpe
 # In[35]:
 
 
-# Y_train_onehot = casualty_to_one_hot(Y_train)
-# Y_test_onehot  = casualty_to_one_hot(Y_test)
+Y_train_onehot = casualty_to_one_hot(Y_train)
+Y_test_onehot  = casualty_to_one_hot(Y_test)
 
-# space={'max_depth': hp.quniform("max_depth", 3, 25, 1),
-#         'gamma': hp.uniform ('gamma', 1,9),
-#         'reg_alpha' : hp.quniform('reg_alpha', 40,180,1),
-#         'reg_lambda' : hp.uniform('reg_lambda', 0,1),
-#         'colsample_bytree' : hp.uniform('colsample_bytree', 0.5,1),
-#         'min_child_weight' : hp.quniform('min_child_weight', 0, 15, 1),
-#         'n_estimators': hp.quniform('n_estimators', 100, 4000, 100),
-#         'tree_method': 'gpu_hist'
-#     }
+space={'max_depth': hp.quniform("max_depth", 3, 25, 1),
+        'gamma': hp.uniform ('gamma', 1,9),
+        'reg_alpha' : hp.quniform('reg_alpha', 40,180,1),
+        'reg_lambda' : hp.uniform('reg_lambda', 0,1),
+        'colsample_bytree' : hp.uniform('colsample_bytree', 0.5,1),
+        'min_child_weight' : hp.quniform('min_child_weight', 0, 15, 1),
+        'n_estimators': hp.quniform('n_estimators', 100, 4000, 100),
+        'tree_method': 'gpu_hist'
+    }
 
-# def objective(space):
-#     clf = XGBClassifier(n_estimators = int(space['n_estimators']),
-#                         max_depth = int(space['max_depth']),
-#                         gamma = space['gamma'],
-#                         reg_alpha = int(space['reg_alpha']),
-#                         min_child_weight = int(space['min_child_weight']),
-#                         colsample_bytree = int(space['colsample_bytree']),
-#                         tree_method = space['tree_method']
-#                        )
+def objective(space):
+    clf = XGBClassifier(n_estimators = int(space['n_estimators']),
+                        max_depth = int(space['max_depth']),
+                        gamma = space['gamma'],
+                        reg_alpha = int(space['reg_alpha']),
+                        min_child_weight = int(space['min_child_weight']),
+                        colsample_bytree = int(space['colsample_bytree']),
+                        tree_method = space['tree_method']
+                       )
     
-#     evaluation = [(X_train, Y_train), (X_test, Y_test)]
+    evaluation = [(X_train, Y_train), (X_test, Y_test)]
     
-#     clf.fit(X_train, Y_train,
-#             eval_set = evaluation, eval_metric = "auc",
-#             early_stopping_rounds = 10, verbose = False)
+    clf.fit(X_train, Y_train,
+            eval_set = evaluation, eval_metric = "auc",
+            early_stopping_rounds = 10, verbose = False)
             
     
-#     pred = clf.predict(X_test)
-#     accuracy = accuracy_score(Y_test, pred>0.5)
-#     print ("SCORE:", accuracy)
-#     return {'loss': -accuracy, 'status': STATUS_OK }
+    pred = clf.predict(X_test)
+    accuracy = accuracy_score(Y_test, pred>0.5)
+    print ("SCORE:", accuracy)
+    return {'loss': -accuracy, 'status': STATUS_OK }
 
 
-# trials = Trials()
+trials = Trials()
 
-# best_hyperparams = fmin(fn = objective,
-#                         space = space,
-#                         algo = tpe.suggest,
-#                         max_evals = 100,
-#                         trials = trials)
+best_hyperparams = fmin(fn = objective,
+                        space = space,
+                        algo = tpe.suggest,
+                        max_evals = 100,
+                        trials = trials)
 
 
 # #### Escritura hiperparámetros
@@ -791,15 +791,15 @@ from hyperopt import STATUS_OK, Trials, fmin, hp, tpe
 # In[ ]:
 
 
-# FILE_NAME = 'leeds_hyperparams' + MODEL_TIMESTAMP + '.json'
+FILE_NAME = 'leeds_hyperparams' + MODEL_TIMESTAMP + '.json'
 
-# write_weights(best_hyperparams, HYPERPARAMS_PATH, FILE_NAME)
+write_weights(best_hyperparams, HYPERPARAMS_PATH, FILE_NAME)
 
 
 # In[ ]:
 
 
-# print(best_hyperparams)
+print(best_hyperparams)
 
 
 # ### Pesos de características
@@ -821,17 +821,17 @@ display(feature_vector)
 # In[39]:
 
 
-# from numpy import loadtxt
-# from xgboost import XGBClassifier,XGBRanker
-# from matplotlib import pyplot
-# from xgboost import plot_importance
+from numpy import loadtxt
+from xgboost import XGBClassifier,XGBRanker
+from matplotlib import pyplot
+from xgboost import plot_importance
 
-# xgboost = XGBClassifier(best_hyperparams, tree_method = 'gpu_hist')
+xgboost = XGBClassifier(best_hyperparams, tree_method = 'gpu_hist')
 
-# xgboost.fit(X_train, Y_train)
+xgboost.fit(X_train, Y_train)
 
-# child_weights  = np.array(xgboost.feature_importances_)
-# feature_vector = fill_feature_vector(X_train, child_weights)
+child_weights  = np.array(xgboost.feature_importances_)
+feature_vector = fill_feature_vector(X_train, child_weights)
 
 
 # #### Visualización pesos calculados
@@ -839,16 +839,16 @@ display(feature_vector)
 # In[ ]:
 
 
-# FILE_PATH = './XGBoost_Weights/'
-# FILE_NAME = 'leeds_figure_weights' + MODEL_TIMESTAMP + '.jpg'
+FILE_PATH = './XGBoost_Weights/'
+FILE_NAME = 'leeds_figure_weights' + MODEL_TIMESTAMP + '.jpg'
 
-# print(xgboost.get_booster().get_score(importance_type= 'weight'))
-# plt.figure(figsize=(10, 5))
-# plt.barh(X_train.columns, xgboost.feature_importances_)
-# plt.savefig(FILE_PATH + FILE_NAME)
+print(xgboost.get_booster().get_score(importance_type= 'weight'))
+plt.figure(figsize=(10, 5))
+plt.barh(X_train.columns, xgboost.feature_importances_)
+plt.savefig(FILE_PATH + FILE_NAME)
 
-# for column, weight in zip(X_train.columns,xgboost.feature_importances_):
-#   print(column, weight)
+for column, weight in zip(X_train.columns,xgboost.feature_importances_):
+  print(column, weight)
 
 
 # #### Escritura de pesos de características
@@ -1117,20 +1117,20 @@ def plot_TSNE(X_data, Y_data, n_components, output_file_name=None):
 # In[ ]:
 
 
-# input_train_shape = (len(array_train_images), 5, 5, 1)
-# input_test_shape = (len(array_test_images), 5, 5, 1)
+input_train_shape = (len(array_train_images), 5, 5, 1)
+input_test_shape = (len(array_test_images), 5, 5, 1)
 
-# array_train_images.reshape(input_train_shape)
-# array_test_images.reshape(input_test_shape)
+array_train_images.reshape(input_train_shape)
+array_test_images.reshape(input_test_shape)
 
-# history = tasp_cnn.fit(array_train_images, Y_train_onehot,
-#                     batch_size = 128, epochs = 100, shuffle = True,
-#                     validation_data = (array_test_images, Y_test_onehot))
+history = tasp_cnn.fit(array_train_images, Y_train_onehot,
+                    batch_size = 128, epochs = 100, shuffle = True,
+                    validation_data = (array_test_images, Y_test_onehot))
 
-# # history
+# history
 
-# device = cuda.get_current_device()
-# device.reset()
+device = cuda.get_current_device()
+device.reset()
 
 
 # ### Escritura del modelo
