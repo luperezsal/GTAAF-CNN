7c3456d65ef96f5266f5fa206196846b7c3c2d98
Luis <balumba55@gmail.com>
2022-04-21 20:26:06 +0200

    [TEST] Add mutation

--- TASP_CNN.py
+++ TASP_CNN.py
@@ -9,7 +9,7 @@
 
 # ## Carga Google Drive
 
-# In[41]:
+# In[1]:
 
 
 # from google.colab import drive
@@ -18,7 +18,7 @@
 
 # ## Versión y especificación de directorios
 
-# In[42]:
+# In[2]:
 
 
 from datetime import datetime
@@ -34,13 +34,13 @@ MODELS_PATH  = 'Models/'
 
 # ## Importar Tensorflow
 
-# In[43]:
+# In[3]:
 
 
 # !pip install tensorflow-addons
 
 
-# In[44]:
+# In[4]:
 
 
 import tensorflow as tf
@@ -55,7 +55,7 @@ from tensorflow.keras.utils import model_to_dot, plot_model
 from tensorflow.keras.layers import Input, Lambda, Activation, Conv2D, MaxPooling2D, BatchNormalization, Add, concatenate, Conv2DTranspose, Flatten
 
 
-# In[45]:
+# In[5]:
 
 
 device_name = tf.test.gpu_device_name()
@@ -67,7 +67,7 @@ get_ipython().system('nvidia-smi')
 
 # ## Importador/Exportador JSON
 
-# In[46]:
+# In[6]:
 
 
 import json
@@ -85,7 +85,7 @@ def load_json(root_path, file_name):
 
 # ## Construcción de imágenes
 
-# In[47]:
+# In[7]:
 
 
 import numpy as np
@@ -165,7 +165,7 @@ def fv2gi(feature_vector):
 
 # ## Construcción Feature Vector
 
-# In[48]:
+# In[8]:
 
 
 def fill_feature_vector(X_dataset,child_weights):
@@ -187,7 +187,7 @@ def fill_feature_vector(X_dataset,child_weights):
 
 # ## Normalización de datos
 
-# In[49]:
+# In[9]:
 
 
 from scipy.stats import zscore
@@ -207,7 +207,7 @@ def normalize_data(X_data):
 
 # ## Oversampling de datos
 
-# In[50]:
+# In[10]:
 
 
 from imblearn.over_sampling import BorderlineSMOTE
@@ -232,7 +232,7 @@ def oversample_data(X_data, Y_labels):
 
 # ## Construcción de imágenes
 
-# In[51]:
+# In[11]:
 
 
 def build_gray_images(dataset, max_dimension, matrix_indexes):
@@ -249,7 +249,7 @@ def build_gray_images(dataset, max_dimension, matrix_indexes):
 
 # ### Inicializar población
 
-# In[52]:
+# In[12]:
 
 
 def initilialize_poplulation(numberOfParents):
@@ -298,7 +298,7 @@ def initilialize_poplulation(numberOfParents):
 
 # ### Fitness function
 
-# In[53]:
+# In[13]:
 
 
 from sklearn.metrics import f1_score
@@ -312,7 +312,7 @@ def fitness_f1score(y_true, y_pred):
 
 # ### Evaluación de población
 
-# In[54]:
+# In[14]:
 
 
 from xgboost import XGBClassifier
@@ -360,7 +360,7 @@ def train_population(population, dMatrixTrain, dMatrixTest, y_test):
 
 # ### Selección de padres
 
-# In[55]:
+# In[15]:
 
 
 import numpy.random as npr
@@ -416,7 +416,7 @@ def new_parents_selection(population, fitness, numParents):
 
 # ### Cruzamiento de población
 
-# In[56]:
+# In[16]:
 
 
 '''
@@ -450,7 +450,7 @@ def crossover_uniform(parents, childrenSize):
 
 # ### Mutación
 
-# In[57]:
+# In[17]:
 
 
 # def mutation(crossover, numberOfParameters):
@@ -505,7 +505,7 @@ def crossover_uniform(parents, childrenSize):
 #     return crossover
 
 
-# In[58]:
+# In[18]:
 
 
 def mutation(crossover, numberOfParameters):
@@ -597,7 +597,7 @@ def mutation(crossover, numberOfParameters):
 
 # ## Reshape de imágenes
 
-# In[59]:
+# In[19]:
 
 
 # Add one channel
@@ -620,7 +620,7 @@ def shape_images(X_data, gray_images):
 
 # ## One-Hot Encoder/Decoder
 
-# In[60]:
+# In[20]:
 
 
 def casualty_to_one_hot(Y_labels):
@@ -650,7 +650,7 @@ def one_hot_to_casualty(Y_labels):
 
 # ### Matriz de correlación
 
-# In[61]:
+# In[21]:
 
 
 import seaborn as sns
@@ -664,7 +664,7 @@ def correlation_matrix(X_data):
 
 # ### PCA
 
-# In[62]:
+# In[22]:
 
 
 from sklearn.decomposition import PCA
@@ -684,7 +684,7 @@ def pca(X_train_data, X_test_data):
 
 # ### TSNE
 
-# In[63]:
+# In[23]:
 
 
 from sklearn.manifold import TSNE
@@ -711,7 +711,7 @@ def plot_TSNE(X_data, Y_data, n_components, output_file_name = None):
 
 # ### Autoencoder
 
-# In[64]:
+# In[24]:
 
 
 def autoencoder ():
@@ -736,7 +736,7 @@ def autoencoder ():
 
 # ## TASP-CNN
 
-# In[65]:
+# In[25]:
 
 
 import tensorflow_addons as tfa
@@ -764,7 +764,7 @@ tasp_cnn.compile(
   )
 
 
-# In[66]:
+# In[26]:
 
 
 print('Done!')
@@ -774,13 +774,13 @@ print('Done!')
 
 # ## Importación de datos
 
-# In[67]:
+# In[27]:
 
 
 # !conda install pandas --y
 
 
-# In[68]:
+# In[28]:
 
 
 import pandas as pd
@@ -834,7 +834,7 @@ a = pd.concat([a, file_2016])
 
 # ## Limpieza de datos
 
-# In[69]:
+# In[29]:
 
 
 ###################### DICCIONARIOS DE REEMPLAZO ######################
@@ -996,13 +996,13 @@ clean_df
 
 # ## Split de datos
 
-# In[70]:
+# In[30]:
 
 
 # !conda install scikit-learn --y
 
 
-# In[71]:
+# In[31]:
 
 
 from sklearn.model_selection import train_test_split
@@ -1019,7 +1019,7 @@ Y_test = test['Casualty Severity']
 
 # ### Downsampling
 
-# In[72]:
+# In[32]:
 
 
 from sklearn.model_selection import train_test_split
@@ -1054,7 +1054,7 @@ X_test_downsampled = downsampled_test.loc[:, ~downsampled_test.columns.isin(['Ca
 Y_test_downsampled = downsampled_test['Casualty Severity']
 
 
-# In[73]:
+# In[33]:
 
 
 # fv2gi(feature_vector)
@@ -1076,13 +1076,13 @@ Y_test_downsampled = downsampled_test['Casualty Severity']
 
 # ## Normalización de datos
 
-# In[74]:
+# In[34]:
 
 
 # !conda install -c conda-forge imbalanced-learn
 
 
-# In[75]:
+# In[35]:
 
 
 X_train = X_train.astype(int)
@@ -1098,7 +1098,7 @@ X_test_downsampled  = normalize_data(X_test_downsampled)
 
 # ## Oversamplig de datos
 
-# In[76]:
+# In[36]:
 
 
 print('********** Before OverSampling **********')
@@ -1112,7 +1112,7 @@ X_train, Y_train = oversample_data(X_train, Y_train)
 
 # ## XGBoost
 
-# In[77]:
+# In[37]:
 
 
 from xgboost import XGBClassifier
@@ -1122,7 +1122,7 @@ from hyperopt import STATUS_OK, Trials, fmin, hp, tpe
 
 # ### Genético
 
-# In[78]:
+# In[38]:
 
 
 # from sklearn.preprocessing import StandardScaler
@@ -1185,14 +1185,14 @@ for generation in range(numberOfGenerations):
                                  childrenSize = (populationSize[0] - parents.shape[0], numberOfParameters))
     
     # Add mutation to create genetic diversity
-    # children_mutated = mutation(children, numberOfParameters)
+    children_mutated = mutation(children, numberOfParameters)
     
     '''
     We will create new population, which will contain parents that where selected previously based on the
     fitness score and rest of them  will be children
     '''
     population[0:parents.shape[0], :] = parents # Fittest parents
-    population[parents.shape[0]:, :]  = children # Children
+    population[parents.shape[0]:, :]  = children_mutated # Children
     
     populationHistory[(generation+1)*numberOfParents : (generation+1)*numberOfParents+ numberOfParents , :] = population # Store parent information
     
