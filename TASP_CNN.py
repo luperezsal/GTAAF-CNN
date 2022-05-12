2803976245f2ad26e95e1fd4bec2271ae406bc14
Luis <balumba55@gmail.com>
2022-05-12 20:29:07 +0200

    [REFACTOR] Comment ipython code lines

--- TASP_CNN.py
+++ TASP_CNN.py
@@ -78,7 +78,7 @@ device_name = tf.test.gpu_device_name()
 if device_name != '/device:GPU:0':
   raise SystemError('GPU device not found')
 print('Found GPU at: {}'.format(device_name))
-get_ipython().system('nvidia-smi')
+# !nvidia-smi
 
 
 # ## Importador/Exportador JSON
