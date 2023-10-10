88fdcadc8872a228f6dca73573154e43a6d0417c
Luis <balumba55@gmail.com>
2023-10-10 13:05:49 +0200

    [REFACTOR] Add more mixins

--- src/mixins/utils.py
+++ src/mixins/utils.py
@@ -22,10 +22,10 @@ def get_feature_matrix_indexes(sorted_feature_vector,matrix):
         else: # Par
             current_row = half_row + normalized_index
 
-        sorted_child_indexes = np.argsort(feature_vector[parent_key]['feature_weights'])[::-1]
+        sorted_child_indexes = np.argsort(sorted_feature_vector[parent_key]['feature_weights'])[::-1]
 
-        child_names   = np.array(feature_vector[parent_key]['feature_childs'])
-        child_weights = np.array(feature_vector[parent_key]['feature_weights'])
+        child_names   = np.array(sorted_feature_vector[parent_key]['feature_childs'])
+        child_weights = np.array(sorted_feature_vector[parent_key]['feature_weights'])
 
         sorted_child_names   = child_names[sorted_child_indexes]
         sorted_child_weights = child_weights[sorted_child_indexes]
@@ -83,15 +83,11 @@ def get_max_child_numbers(feature_vector):
 
 	return max_child_numbers
 
+def build_gray_images(dataset, dimension_x, dimension_y, matrix_indexes):
 
-def build_gray_images(dataset, max_dimension, matrix_indexes):
-
-    matrix_3d = np.zeros((max_dimension, max_dimension, len(dataset.index)))
+    matrix_3d = np.zeros((dimension_x, dimension_y, len(dataset.index)))
     print(len(dataset.index))
     for feature, value in matrix_indexes.items():
         matrix_3d[value[0], value[1],] = dataset[feature]
         
     return matrix_3d
-
-
-# matrix_indexes = fv2gi(feature_vector)
